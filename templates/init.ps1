4afbea0d0124449a5645e525dc7c3396cd156032
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2021-08-02 23:25:49 -0500

    Added 'init' template

---
+++ templates/init.ps1
@@ -0,0 +1,15 @@
+[cmdletbinding()]
+Param (
+    [string]$parameter1,
+    [string]$parameter2
+)
+
+
+##########  INIT  ###############################################
+
+$ErrorActionPreference = 'Stop'
+$SCRIPT_DIR = $PSScriptRoot
+$SCRIPT_NAME = $MyInvocation.MyCommand.Name
+$WORKSPACE = $PWD.Path
+$IS_VERBOSE = $PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent
+if (!$IS_VERBOSE) { $IS_VERBOSE = $false }
