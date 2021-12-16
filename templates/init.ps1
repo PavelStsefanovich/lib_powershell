35cb52c35b174dd6f1f9fa6284b8c83119b6800c
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2021-12-16 17:15:55 -0600

    Fixed IS_INTERACTIVE flag

--- templates/init.ps1
+++ templates/init.ps1
@@ -32,8 +32,8 @@ $SCRIPT_DIR = $PSScriptRoot
 $SCRIPT_NAME = $MyInvocation.MyCommand.Name
 $WORKSPACE = $PWD.Path
 $IS_VERBOSE = [bool]($PSCmdlet.MyInvocation.BoundParameters.Verbose)
-$IS_INTERACTIVE = $cmd = (Get-CimInstance win32_process -Filter "ProcessID=$PID" | `
-    ? { $_.processname -eq "powershell.exe" }).CommandLine -like  "*-NonI*"
+$IS_INTERACTIVE = (Get-CimInstance win32_process -Filter "ProcessID=$PID" | `
+        ? { $_.processname -eq "powershell.exe" }).CommandLine -notlike "*-NonI*"
 
 
 #--------------------------------------------------
